3fd47068dc0a442ef2d6c919e8edc5838465cb07
AvhiMaz <avhimazumder5@outlook.com>
2023-07-25 14:40:56 +0530

    unknown

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <p class="underline decoration-1 ...">The quick brown fox...</p>
+      <p class="underline decoration-2 ...">The quick brown fox...</p>
+      <p class="underline decoration-4 ...">The quick brown fox...</p>
     </div>
   );
 }

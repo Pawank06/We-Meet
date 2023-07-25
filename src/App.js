9e43aba98bd34a466fa0c168de0c5e0be9ea1fa7
“Pawank06” <pawan06kumar2003@gmail.com>
2023-07-25 20:31:23 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <Navbar/>
+    {/* <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
       New meeting
-</button>
+</button> */}
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+const Navbar = () => {
+    return(
+        <>
+        <div className="meet-nav flex flex-row m-4">
+            <img  className="p-4 pr-1" src="./img/wemate.png" alt=""/>
+            <p className=" pt-6 pl-0 pr-0 pb-6 text-gray-500">Meet</p>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;

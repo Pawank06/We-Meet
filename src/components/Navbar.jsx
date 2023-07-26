1e9c865bae49ce4a4c5747ddb46e3befb9937b4d
“Pawank06” <pawan06kumar2003@gmail.com>
2023-07-26 11:03:21 +0530

    done navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ import DateTime from "./DateTime";
 const Navbar = () => {
     return(
         <>
-        <div className="grid grid-cols-6 gap-4">
-        <div className=" col-start-1 col-end-3  meet-nav flex flex-row m-4">
-            <img  className="" src="./img/wemate.png" alt=""/>
-            <p className=" p-0 pl-1 text-gray-500">Meet</p>
+        <div className="flow-root">
+        <div className=" float-left flex flex-row m-4">
+            <img  className="p-4 pr-0" src="./img/wemate.png" alt=""/>
+            <p className=" p-4 pl-1 text-gray-500">Meet</p>
         </div>
-        <div className=" m-4 col-end-7 col-span-1 text-gray-500">
-            <p><DateTime/></p>
+        <div className=" float-right m-4 text-gray-500">
+            <p className="m-4"><DateTime/></p>
         </div>
         </div>
         </>

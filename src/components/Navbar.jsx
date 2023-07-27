786709f90ffc6c41f1e0c082d523fc1f3f848e29
AvhiMaz <avhimazumder5@outlook.com>
2023-07-27 21:08:15 +0530

    navbar 80% completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import DateTime from "./DateTime";
+import {AiOutlineQuestionCircle} from "react-icons/ai"
+import {BiCommentError} from "react-icons/bi"
+import {AiOutlineSetting} from "react-icons/ai"
 
 
 
@@ -8,12 +11,15 @@ const Navbar = () => {
     return(
         <>
         <div className="flow-root">
-        <div className=" float-left flex flex-row m-4">
+        <div className=" float-left flex flex-row m-1">
             <img  className="p-4 pr-0" src="./img/wemate.png" alt=""/>
-            <p className=" p-4 pl-1 text-gray-500">Meet</p>
+            <p className=" meet-text p-4 pl-1 text-gray-500">Meet</p>
         </div>
-        <div className=" float-right m-4 text-gray-500">
-            <p className="m-4"><DateTime/></p>
+        <div className="flex flex-row float-right m-2 text-gray-500">
+            <p className=" date-time m-4 mr-4 "><DateTime/></p>
+            <p className="meet-text m-4 ml-0 support-icon"><AiOutlineQuestionCircle/></p>
+            <p className="meet-text m-4 ml-0 support-icon"><BiCommentError/></p>
+            <p className="meet-text m-4 ml-0 support-icon"><AiOutlineSetting/></p>
         </div>
         </div>
         </>

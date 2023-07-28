71d429b289f1ff684259885556c8a806fc96442f
“Pawank06” <pawan06kumar2003@gmail.com>
2023-07-28 09:06:53 +0530

    80% completed 1st grid

--- src/components/BodyContent.jsx
+++ src/components/BodyContent.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import {MdKeyboard} from "react-icons/md";
+import {BiVideoPlus} from "react-icons/bi";
 const BodyContent = () => {
     return (
         <>
-            <div className="container-body grid grid-cols-12 ">
+            <div className="container-body grid p-12 grid-cols-12 ">
                 <div className="col-span-7 m-12">
                     <div className="pt-4 pb-4">
                         <div className="ml-12 mr-12">
@@ -14,11 +15,35 @@ const BodyContent = () => {
                             <div className=" mr-12"><p className="meet-text-2">We re-engineered the service we built for sacure business meetings, Google Meet, to make it free and available to all.
                             </p></div>
 
-                            <div>
-                                <button class="meet-text-3 m-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <div className="flex items-center">
+
+                                
+
+                                <button class="flex items-center meet-text-3 m-4 ml-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    <p className="p-2 pl-0"><BiVideoPlus /></p>
+                                
                                     New meeting
                                 </button>
 
+                                <div class="flex items-center">
+                  <form method="GET">
+                    <div class="relative text-gray-600">
+                      <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <button
+                          type="submit"
+                          class="p-1"
+                        >
+                          <p className="text-2xl"><MdKeyboard/></p>
+                        </button>
+                      </span>
+                      <input
+                        type="search"
+                        className="placeholder-gray-600 border-2 border-gray-400  rounded-[4px] pl-10 p-3"
+                        placeholder ="Enter a code or link"
+                      />
+                    </div>
+                  </form>
+                </div>
                             </div>
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,20 +10,20 @@ import {AiOutlineSetting} from "react-icons/ai"
 const Navbar = () => {
     return(
         <>
-        
+
         <div className="grid grid-cols-12">
-        <div className="flex items-center col-span-7">
+        <div className="flex items-center col-span-9 ml-2">
             <img  className="" src="./img/wemate.png" alt=""/>
             <p className=" meet-text text-gray-500">Meet</p>
         </div>
-        <div className="flex items-center col-span-5 text-gray-500">
+        <div className="flex items-center col-span-3 text-gray-500">
             <p className=" date-time m-4 mr-4 "><DateTime/></p>
             <p className="meet-text m-4 ml-0 support-icon"><AiOutlineQuestionCircle/></p>
             <p className="meet-text m-4 ml-0 support-icon"><BiCommentError/></p>
             <p className="meet-text m-4 ml-0 support-icon"><AiOutlineSetting/></p>
         </div>
         </div>
-        
+
         </>
     )
 }

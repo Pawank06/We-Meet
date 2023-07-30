201fe1cef1a200ec4c0ff1b507382207b9af6cb9
pawan <pawan@pop-os.localdomain>
2023-07-30 15:55:13 +0530

    fixed alignment of slider

--- src/components/SliderComponent.jsx
+++ src/components/SliderComponent.jsx
@@ -1,33 +1,24 @@
-// import React, {useState } from 'react';
 // Import Swiper React components
-import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
 
-
-// import required modules
-import { Navigation } from 'swiper/modules';
-
-export default function App() {
+const SliderComponent = () => {
   return (
-    <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-
-        <SwiperSlide className=''>
-            <img src="./img/g-meet-0.svg" alt="" />
-        </SwiperSlide>
-
-        <SwiperSlide >
-          <img src="./img/g-meet0.5.svg" alt="" />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <img src="./img/g-meet-1.svg" alt="" />
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <div>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide><img src="./img/g-meet-0.svg" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="./img/g-meet0.5.svg" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="./img/g-meet-1.svg" alt="" /></SwiperSlide>
+    </Swiper>
+    </div>
   );
-}
+};
+
+export default SliderComponent;

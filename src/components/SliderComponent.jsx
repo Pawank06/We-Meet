deb72ed4f5329ece13d191d3ee6fef85068b4203
Pawank06 <pawan06kumar2003@gmail.com>
2023-07-31 11:35:56 +0530

    slider alignment completed

--- src/components/SliderComponent.jsx
+++ src/components/SliderComponent.jsx
@@ -9,16 +9,36 @@ import 'swiper/css';
 const SliderComponent = () => {
   return (
     <div>
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide><img src="./img/g-meet-0.svg" alt="" /></SwiperSlide>
-      <SwiperSlide><img src="./img/g-meet0.5.svg" alt="" /></SwiperSlide>
-      <SwiperSlide><img src="./img/g-meet-1.svg" alt="" /></SwiperSlide>
-    </Swiper>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div className='ss'>
+            <img src="./img/g-meet-0.svg" alt="" />
+
+            <h2 className='slider-header m-2'>Get a link you can share</h2>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className='ss'>
+            <img src="./img/g-meet0.5.svg" alt="" />
+            <h2 className='slider-header m-2'>Plan ahead</h2>
+          </div>
+
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className='ss'>
+            <img src="./img/g-meet-1.svg" alt="" />
+            <h2 className='slider-header m-2'>Your meeting is safe</h2>
+          </div>
+
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
